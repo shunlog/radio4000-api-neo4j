02c2a08997653cf539f7bd262eb39d81edf5ccf6
Artiom White <48106195+Kyle-is-online@users.noreply.github.com>
2021-03-27 23:13:53 +0200

    Add models

---
+++ models.py
@@ -0,0 +1,34 @@
+from neomodel import (StructuredNode, StringProperty, IntegerProperty,
+                      UniqueIdProperty, RelationshipTo, RelationshipFrom,
+                      DateTimeFormatProperty, FloatProperty, StructuredRel,
+                      BooleanProperty, DateProperty)
+
+class TagRel(StructuredRel):
+    channel = StringProperty(required=True)
+
+class Track(StructuredNode):
+    uid = UniqueIdProperty()
+    url = StringProperty(unique_index=True, required=True)
+    title = StringProperty()
+    body = StringProperty()
+    media_not_available = BooleanProperty(default=False)
+    created = DateProperty()
+    discogs_url = StringProperty()
+    tags = RelationshipTo('Tag', 'TAGGED', model=TagRel)
+
+class Channel(StructuredNode):
+    uid = UniqueIdProperty()
+    slug = StringProperty(unique_index=True, required=True)
+    title = StringProperty()
+    body = StringProperty()
+    created = DateProperty()
+    image = StringProperty()
+    is_featured = BooleanProperty(default=False)
+    link = StringProperty()
+    updated = DateProperty()
+    follows = RelationshipTo('Channel', 'FOLLOWS')
+    likes = RelationshipTo('Track', 'LIKES')
+
+class Tag(StructuredNode):
+    uid = UniqueIdProperty()
+    name = StringProperty(unique_index=True, required=True)
